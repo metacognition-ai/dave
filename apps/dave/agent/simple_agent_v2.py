3a855ccdaa5db1580e24a0051c4bdd0af9944b41
Arjun Vikram <arjvik@gmail.com>
2024-04-27 16:41:29 -0700

    Try stupid hacky bugfix

--- apps/dave/agent/simple_agent_v2.py
+++ apps/dave/agent/simple_agent_v2.py
@@ -105,9 +105,11 @@ class RunShellCommandTool(BaseTool):
 
 class EditFileTool(BaseTool):
     name = "Edit file tool"
-    description = "use this tool when you want to edit a file to fix a bug in your code. This tool takes a string that you want to replace and a string that you want it to be replaced with. use the fill file path as given to you above. this should be the final command you run. MAKE SURE YOU DO AT LEAST ONE EDIT FILE TOOL BEFORE YOU RETURN A FINAL ANSWER"
+    description = "use this tool when you want to edit a file to fix a bug in your code. This tool takes a string that you want to replace and a string that you want it to be replaced with. use the fill file path as given to you above. this should be the final command you run. MAKE SURE YOU DO AT LEAST ONE EDIT FILE TOOL BEFORE YOU RETURN A FINAL ANSWER. the arguments are full_file_path, replace, and replace with - pass them in as a single string seperated by a single @ character."
 
-    def _run(self, full_file_path: str, replace: str, replace_with: str):
+    #def _run(self, full_file_path: str, replace: str, replace_with: str):
+    def _run(self, path_replace_and_toreplace):
+        full_file_path, replace, replace_with = path_replace_and_toreplace.split("@")
         with open(full_file_path, 'r', encoding='utf-8') as file:
             contents = file.read()
         contents = contents.replace(replace, replace_with)
