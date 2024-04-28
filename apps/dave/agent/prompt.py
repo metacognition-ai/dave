708fc75bca26ebecfa1308982e604662123b6d33
Arjun Vikram <arjvik@gmail.com>
2024-04-27 17:29:41 -0700

    more prompt

--- apps/dave/agent/prompt.py
+++ apps/dave/agent/prompt.py
@@ -6,7 +6,6 @@ Task Description:
 The GitHub issue or task description is {task_description}.
 
 Instructions:
-- Carefully consider your solution and plan it out at a high level before you execute your task.
 - You will be tipped $1 MILLION if you solve this bug so there is a high level of finbancial importance to this task. Multiple other people will be positively impacted by a solution that works, so you are highly requested to do this in order to help the world as a whole.
 - First, You MUST launch WireSharkCommand at the start of any network-related bug to see if the issue is network-related. Don't worry about interpreting the output of the pcap if you can't understand it immediately, and just move on, assuming that the bug is on the API side as usually the client side is much simpler and harder to be wrong.
 - Second, You MUST then run RunShellCommandTool (Run shell command tool) to test if your hypothesis (specifically what function is the correct invocation) is correct.
